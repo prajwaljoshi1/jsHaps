95e0d2da8fd8d0cece29b43604717c054beac8a5
Prajwal Joshi <prazwal@outlook.com>
2016-03-30 01:14:24 +1100

    complete article curd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,59 @@
 class ArticlesController < ApplicationController
 
-  def  new
+before_action :set_article, only: [:edit, :update, :show, :destory]
+
+  def index
+    @articles = Article.all
+  end
+
+
+  def new
     @article = Article.new
   end
 
-  def create
-      render  params[:article].inspect
-      @article = Article.new(article_params)
-      @article.save
-      redirect_to articles_show(@articles)
-
-      
+  def edit
   end
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] ="Article was successfully created"
+      redirect_to article_path(@article)
+      #do smthng
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+  end
+end
+
+
+  def show
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article Deleted successfully"
+    redirect_to articles_path
+  end
+
 
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
