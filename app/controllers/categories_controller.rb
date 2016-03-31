8d52f9c71f23acb010e17dd69ecb14c09ea8247e
Prajwal Joshi <prazwal@outlook.com>
2016-03-31 22:48:05 +1100

    finish categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,21 @@ before_action :require_admin, except: [:index, :show]
     end
   end
 
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+      @category = Category.find params[:id]
+      if @category.update category_params
+        flash[:success] = "Category was successfully updated."
+        redirect_to category_path(@category)
+      else
+        render 'edit'
+      end
+
+  end
+
   def show
     @category = Category.find params[:id]
     @category_articles = @category.articles.paginate(page: params[:page], per_page:5)
