542be30007c75e5ee5a805020687e4eaf4d00599
Prajwal Joshi <prazwal@outlook.com>
2016-03-31 21:29:57 +1100

    finish categories change theme

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,11 @@ class Article < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
 
   validates  :title, presence: true, length: { minimum: 3 ,maximum: 70}
-  validates  :description, presence: true, length: { minimum: 20 ,maximum: 1000}
+  validates  :description, presence: true, length: { minimum: 20 ,maximum: 2000}
   validates  :user_id, presence: true
 end
