a217f2478eba32dc70c1419130b654354f533d4a
Prajwal Joshi <prazwal@outlook.com>
2016-03-31 13:18:03 +1100

    complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?  # this enables these methods to be available in view
+  helper_method :current_user, :logged_in?  # this enables these methods to be available in all view and controller
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  #returns user object
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :require_same_user, only: [:edit, :update,]
+
   def new
     @user = User.new
   end
@@ -45,6 +47,13 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your account"
+      redirect_to root_path
+    end
   end
 
 end
